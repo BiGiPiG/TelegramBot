711c6e68903804eb5f8c9984b371e4d2f158a41a
BiGPiG <Bogdan.ars.15@yandex.ru>
2025-05-14 22:55:39 +0300

    created MessageService

---
+++ StockSmartBot/src/main/java/io/github/bigpig/services/MessageService.java
@@ -0,0 +1,127 @@
+package io.github.bigpig.services;
+
+import io.github.bigpig.dto.ShareDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+
+    public String generateStartCommand() {
+        String startText = """
+        🚀 *Добро пожаловать в MarketInsightBot!* 🚀
+
+        *Ваш персональный помощник для анализа рынков:*
+        - 📊 Ключевые метрики (P/E, P/S и др.)
+        - 🤖 AI-анализ
+        - 📈 Визуализация данных
+
+        🔍 *Как начать?*
+        Просто введите команду с тикером интересующего актива:
+
+        `/getValuationMetrics MSFT` - анализ Microsoft
+        `/getSmartAnalyse MSFT` - AI-разбор Microsoft
+
+        📌 Для всех команд напишите /help
+        """;
+
+        return escapeMarkdownSymbols(startText);
+    }
+
+    public String generateHelpCommand() {
+        String helpText = """
+        📊 *Bot Commands Help* 📊
+        
+        *MarketInsightBot* -Your Market Analysis Assistant:
+        - 📈 Stocks, ETFs and Cryptocurrencies
+        - 🤖 AI-аналитика и прогнозирование
+        - 🔍 Ключевые финансовые метрики
+        
+        🔹 *Основные команды*
+        /start - Начать работу
+        /help - Справка по командам
+        
+        📈 *Анализ активов*
+        /getValuationMetrics [тикер] - Основные метрики (P/E, P/S и др.)
+        
+        🤖 *AI-аналитика*
+        /getSmartAnalyse [тикер] - Анализ с искусственным интеллектом
+        
+        🖼 *Визуализация данных*
+        /getChart [тикер]
+        
+        📌 *Примеры запросов*
+        `/getValuationMetrics AAPL` - анализ Apple
+        `/getSmartAnalyse AAPL` - AI-разбор Apple
+        `/getChart AAPL` - график для Apple
+        """;
+
+        return escapeMarkdownSymbols(helpText);
+    }
+
+    public String generateGetValuationMetricsCommand(ShareDTO curShare) {
+        String valuationMetricsText = String.format("""
+                    📈 *%s*:
+                  
+                    Price: `%s`
+             
+                    *Description*
+                 
+                    %s
+                 
+                    💹 *Daily Price Metrics*
+                  
+                    ▫️ High: `%s`
+                    ▫️ Low: `%s`
+                    ▫️ Change: `%s (%s%%)`
+                    ▫️ Volume: `%s`
+
+                    📊 *Fundamental Metrics*
+
+                    P/E (Price/Earn): %s
+                    P/B (Price/Book): %s
+                    P/S (Price/Sale): %s
+                    Market Capitalisation: %s
+                    EPS: %s
+                    Book Value: %s
+                  """,
+                String.format("%s", curShare.getName()),
+                String.format("%s", curShare.getGlobalQuote().getCurrentPrice()),
+                String.format("%s", curShare.getDescription().replace("'", "").replace("`", "")),
+                String.format("%s", curShare.getGlobalQuote().getHighPrice()),
+                String.format("%s", curShare.getGlobalQuote().getLowPrice()),
+                String.format("%s", curShare.getGlobalQuote().getPriceChange()),
+                curShare.getGlobalQuote().getChangePercent().replace("%", ""),
+                String.format("%s", curShare.getGlobalQuote().getVolume()),
+                String.format("%.2f", curShare.getPeRatio()),
+                String.format("%.2f", curShare.getPbRatio()),
+                String.format("%.2f", curShare.getPriceToSales()),
+                curShare.getMarketCap(),
+                String.format("%.2f", curShare.getEps()),
+                String.format("%.2f", curShare.getBookValue())
+        );
+
+        return escapeMarkdownSymbols(valuationMetricsText);
+    }
+
+    public String generateProcessingMessage(int dotCount) {
+        String dots = ".".repeat(dotCount % 4); // от 0 до 3
+        return escapeMarkdownSymbols("⏳ Запрос обрабатывается" + dots);
+    }
+
+    public String generateSmartAnalyseResult(String result) {
+        return escapeMarkdownSymbols(result.substring(1, result.length() - 1));
+    }
+
+    public String generateErrorMessage(String msg) {
+        return escapeMarkdownSymbols(msg);
+    }
+
+    public String escapeMarkdownSymbols(String text) {
+        text = text.replace("\\n", "\n");
+        String[] symbols = {"_", "[", "]", "(", ")", "~", ">", "#", "+", "-", "=", "|", ".", "!", ":"};
+        for (String symbol : symbols) {
+            text = text.replace(symbol, "\\" + symbol);
+        }
+        return text.replace("**", "*");
+    }
+}
